f51b1eb5327d7cf30bf82b85718185433c61f133
Lidiya <lidiya.karn@gmail.com>
2022-04-12 22:24:24 +0600

    removed projects

--- Homework 1. Task 1./Pods/Target Support Files/Pods-Homework 1. Task 1./Pods-Homework 1. Task 1.-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Homework_1__Task_1_ : NSObject
-@end
-@implementation PodsDummy_Pods_Homework_1__Task_1_
-@end
--- Homework 1. Task 2./Pods/Target Support Files/Pods-Homework 1. Task 2./Pods-Homework 1. Task 2.-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Homework_1__Task_2_ : NSObject
-@end
-@implementation PodsDummy_Pods_Homework_1__Task_2_
-@end
